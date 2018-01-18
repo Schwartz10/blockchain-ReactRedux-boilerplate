2afe585145405e4d6450d0ee93c975961e0a5778
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-01-18 18:23:40 -0500

    adds post and postLottery to user model

--- contracts/CapCoin.sol
+++ contracts/CapCoin.sol
@@ -4,6 +4,7 @@ contract CapCoin {
 
   event NewUser(string name);
   event BoughtTokens(uint coinBalance);
+  event CreatedPost(string url);
 
   uint coinSupply = 1000000;
   uint coinsBought = 0;
@@ -16,10 +17,18 @@ contract CapCoin {
   struct User {
     string name;
     uint coinBalance;
+    uint postNum;
+  }
+
+  struct Post {
+    string url;
+    uint lotteryAmount;
   }
 
   mapping (address => User) public addressToUser;
+  mapping (address => Post) public addressToPost;
   address[] public users;
+  address[] public posts;
 
   function createUser(string name) public {
     var user = addressToUser[msg.sender];
@@ -35,8 +44,11 @@ contract CapCoin {
     return users;
   }
 
-  function getUser() public view returns (string, uint) {
-    return (addressToUser[msg.sender].name, addressToUser[msg.sender].coinBalance);
+  function getUser() public view returns (string, uint, string, uint) {
+    return (addressToUser[msg.sender].name,
+      addressToUser[msg.sender].coinBalance,
+      addressToPost[msg.sender].url,
+      addressToPost[msg.sender].lotteryAmount);
   }
 
   function buyTokens(uint amount) public {
@@ -45,4 +57,15 @@ contract CapCoin {
     coinSupply -= amount;
     BoughtTokens(addressToUser[msg.sender].coinBalance);
   }
+
+  function createPost(string url) public {
+    // 5 coins per post
+    var post = addressToPost[msg.sender];
+    post.url = url;
+    post.lotteryAmount = 5;
+    addressToUser[msg.sender].coinBalance -= 5;
+    CreatedPost(url);
+  }
+
+  function() internal payable {}
 }
