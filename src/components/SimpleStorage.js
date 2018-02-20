df7c3c6d330d292683348cb8b5b367d4605817ba
Jonathan Schwartz <jpschwartz2@uwalumni.com>
2018-02-20 12:08:08 -0500

    successfully fetches and reports value in the simple storage smart contract

--- src/components/SimpleStorage.js
+++ src/components/SimpleStorage.js
@@ -1,8 +1,52 @@
-import React from 'react'
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
 
-const SimpleStorage = () =>
-  <div>
-    <h1>test</h1>
-  </div>
+/*
+You know you've correctly set up your environment if your simple storage contract is working - you should be able to set and get the value in your smart contract. If not, somethings not right
+*/
 
-export default SimpleStorage;
+class SimpleStorage extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      value: 0,
+      dirty: false
+    }
+  }
+
+  componentDidMount(){
+    this.setState({ dirty: false })
+  }
+
+  componentWillUpdate(nextProps, nextState){
+    // checks to see if we've fetched the necessary information from the blockchain
+    if (!this.state.dirty && nextProps.accounts.length && Object.keys(nextProps.contract).length) {
+      nextProps.contract.get.call(nextProps.accounts[0])
+      .then(result => this.setState({ value: result.c[0], dirty: true }))
+    }
+  }
+
+  set(number){
+
+  }
+
+  render(){
+    return(
+      <div>
+        <h1>The number stored in your smart contract is: <br /> {this.state.value} </h1>
+      </div>
+    )
+  }
+}
+
+/**
+ * CONTAINER
+ */
+const mapState = (state) => {
+  return {
+    contract: state.contract,
+    accounts: state.accounts
+  }
+}
+
+export default connect(mapState)(SimpleStorage)
